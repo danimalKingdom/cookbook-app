bec65497784d459a99bfd067b49f6be82e17c655
dani <dani.zaghian@gmail.com>
2017-01-24 18:46:33 -0800

    added model methods

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,29 @@
 class Recipe < ActiveRecord::Base
+  def ingredients_list
+    return ingredients.split(",")
+  end
+
+  def directions_list
+    return directions.split(",")
+  end
+
+  def friendly_created_at
+    created_at.strftime("%b %e, %l:%M %p")
+  end
+
+  def friendly_prep_time
+    hours = prep_time / 60
+    minutes = prep_time%60
+    result = ""
+    result += "#{hours} hours" if hours >= 1
+    result += "#{minutes} minutes" if minutes > 0
+    return result
+  end
 end
+
+
+
+
+
+
+
